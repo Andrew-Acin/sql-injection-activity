b90df90591b1e0c4a1674dd70bae6b50f2b52ca0
Andrew Acin <andrew_acin@hotmail.com>
2024-08-06 15:39:42 -0700

    got the injection working

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 
 
-Given what you already know about SQL, can you spot our vulnerability?
+<!-- Given what you already know about SQL, can you spot our vulnerability? -->
+it is vulnerable to SQL injection in the password field 
 
 
 
+<!-- Mount the attack
+What happened? -->
+i got an error has occurred message......
 
-Mount the attack
-What happened?
+Second try. This time it worked, i had to restart the server. It gave me access as Administrator
 
 
-
-Taking your best guess, what do you think happened?
+<!-- Taking your best guess, what do you think happened? -->
+i entered unknown' OR '1'='1 into the password field and i think the first ' after unknown helped close the stirng for password and open up the OR '1'='1' which gave us a truthy value giving us access to the login.
--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const db = new sqlite3.Database(':memory:');
 db.serialize(function () {
     db.run("CREATE TABLE user (username TEXT, password TEXT, title TEXT)");
     db.run("INSERT INTO user VALUES ('privilegedUser', 'privilegedUser1', 'Administrator')");
+    db.run("INSERT INTO user VALUES ('user', 'password', 'User')");
 });
 
 
@@ -35,14 +36,14 @@ app.post('/login', function (req, res) {
 
         db.get(query, function (err, row) {
                 
-        if (err) {
-            console.log('ERROR', err);
-            res.redirect("/index.html#error");
-        } else if (!row) {
-            res.redirect("/index.html#unauthorized");
-        } else {
-            res.send('Hello <b>' + row.title + '!</b><br /> This file contains all your secret data: <br /><br /> SECRETS <br /><br /> MORE SECRETS <br /><br /><a href="/index.html">Go back to login</a>');
-        }
+		if (err) {
+			console.log('ERROR', err);
+			res.redirect("/index.html#error");
+		} else if (!row) {
+			res.redirect("/index.html#unauthorized");
+		} else {
+			res.send('Hello <b>' + row.title + '!</b><br /> This file contains all your secret data: <br /><br /> SECRETS <br /><br /> MORE SECRETS <br /><br /> <a href="/index.html">Go back to login</a>');
+		}
     });
 
 })
